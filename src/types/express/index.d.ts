2625da4e2f661929754340013164cb3058052a5d
bayazidhassan <bayazidhassan776@gmail.com>
2026-09-04 00:53:50 +0600

    authenticate

---
+++ src/types/express/index.d.ts
@@ -0,0 +1,17 @@
+import 'express';
+
+declare global {
+  namespace Express {
+    interface UserPayload {
+      id: string;
+      name: string;
+      email: string;
+    }
+
+    interface Request {
+      user: UserPayload;
+    }
+  }
+}
+
+export {};
